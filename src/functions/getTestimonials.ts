56992f4e61e9132d18dec0eb23cbefdb015abbab
Franck Siguier <franck.siguier@hungryhungry.com>
2025-11-06 17:54:19 +1100

    make static

--- src/functions/getTestimonials.ts
+++ src/functions/getTestimonials.ts
@@ -2,13 +2,16 @@ import { createServerFn } from "@tanstack/react-start";
 import { db } from "~/db/db";
 import { testimonial } from "~/db/schema";
 import { sampleTestimonials } from "~/db/samples/testimonials";
+import { staticFunctionMiddleware } from "@tanstack/start-static-server-functions";
 
 export const getTestimonials = createServerFn({
   method: "GET",
-}).handler(async () => {
-  const testimonials = await db.select().from(testimonial);
-  if (testimonials.length === 0) {
-    return { testimonials: sampleTestimonials };
-  }
-  return { testimonials };
-});
+})
+  .middleware([staticFunctionMiddleware])
+  .handler(async () => {
+    const testimonials = await db.select().from(testimonial);
+    if (testimonials.length === 0) {
+      return { testimonials: sampleTestimonials };
+    }
+    return { testimonials };
+  });
